efaa957a39de0fe7c1a6465ef885144ed6b6cf38
Matheus Gabriel <tesch.matheusg@gmail.com>
2026-04-02 22:25:45 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'app/views/splash_page.dart';
+import 'app/views/login_page.dart';
+import 'app/views/signup_page.dart';
+import 'app/views/home_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashPage(),
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
