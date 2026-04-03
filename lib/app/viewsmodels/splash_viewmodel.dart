46ce8120c67082e7b32e93cf9d8712c8ad281329
thiagodomingues-stack <thiago.domingues@sou.unifeob.edu.br>
2026-04-02 22:13:53 -0300

    Update splash_viewmodel.dart

--- lib/app/viewsmodels/splash_viewmodel.dart
+++ lib/app/viewsmodels/splash_viewmodel.dart
@@ -1,1 +1,15 @@
+import 'dart:async';
 
+// Responsável pela lógica da SplashScreen
+class SplashViewModel {
+
+  // Método que executa o delay e chama navegação
+  Future<void> iniciar(Function navegar) async {
+
+    // Aguarda 3 segundos
+    await Future.delayed(Duration(seconds: 3));
+
+    // Executa a função de navegação passada como parâmetro
+    navegar();
+  }
+}
