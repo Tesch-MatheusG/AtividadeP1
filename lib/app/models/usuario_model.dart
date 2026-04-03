fc3ba5ffbeb7b3b9f84b25581112b8864dd74582
polianavela <poliana.vela@sou.unifeob.edu.br>
2026-04-02 22:11:43 -0300

    Update usuario_model.dart

--- lib/app/models/usuario_model.dart
+++ lib/app/models/usuario_model.dart
@@ -1,1 +1,14 @@
+// Model representa a estrutura de dados do usuário
+class UsuarioModel {
+  // Atributos do usuário
+  final String nome;
+  final String email;
+  final String senha;
 
+  // Construtor obrigatório para criar um usuário
+  UsuarioModel({
+    required this.nome,
+    required this.email,
+    required this.senha,
+  });
+}
