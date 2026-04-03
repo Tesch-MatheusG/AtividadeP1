b1c074911605b4a808ec06f51295c3992ef282fd
polianavela <poliana.vela@sou.unifeob.edu.br>
2026-04-02 22:12:04 -0300

    Update usuario_mock_store.dart

--- lib/app/data/usuario_mock_store.dart
+++ lib/app/data/usuario_mock_store.dart
@@ -1,1 +1,27 @@
+import '../models/usuario_model.dart';
 
+ // Simula um banco de dados em memória enquanto o app está rodando
+class UsuarioMockStore {
+
+  // Lista estática que armazena os usuários enquanto o app está rodando
+
+static List<UsuarioModel> _usuarios = [];
+
+  // Método para adicionar um novo usuário na lista
+  static adicionarUsuario(UsuarioModel usuario) {
+    _usuarios.add(usuario);
+  }
+
+  // Método para autenticar usuário
+  static UsuarioModel? autenticar(String email, String senha) {
+    try {
+      // Procura um usuário com email e senha iguais
+      return _usuarios.firstWhere(
+        (user) => user.email == email && user.senha == senha,
+      );
+    } catch (e) {
+      // Caso não encontre, retorna null
+      return null;
+    }
+  }
+}
