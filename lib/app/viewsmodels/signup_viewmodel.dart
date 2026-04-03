f4ddd7fe070b1a2871f5009e1906d8513317b28c
thiagodomingues-stack <thiago.domingues@sou.unifeob.edu.br>
2026-04-02 22:13:41 -0300

    Implement signup logic in SignupViewModel

--- lib/app/viewsmodels/signup_viewmodel.dart
+++ lib/app/viewsmodels/signup_viewmodel.dart
@@ -1,1 +1,20 @@
+import '../data/usuario_mock_store.dart';
+import '../models/usuario_model.dart';
 
+// Responsável pela lógica da tela de cadastro
+class SignupViewModel {
+
+  // Método para cadastrar usuário
+  void cadastrar(String nome, String email, String senha) {
+
+    // Cria um novo objeto UsuarioModel
+    final usuario = UsuarioModel(
+      nome: nome,
+      email: email,
+      senha: senha,
+    );
+
+    // Adiciona o usuário na lista mockada
+    UsuarioMockStore.adicionarUsuario(usuario);
+  }
+}
